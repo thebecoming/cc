5db58cde4e62da6434e57886e1b9b40b0b336418
thebecoming <derekyoussi@gmail.com>
2018-08-11 11:16:24 -0700

    tmp

--- pullDigline.lua
+++ pullDigline.lua
@@ -6,22 +6,16 @@ function InitProgram()
 
     shell.run("delete util")
     shell.run("wget https://raw.githubusercontent.com/thebecoming/cc/master/util.lua util")
+
+    shell.run("delete equip")
+    shell.run("wget https://raw.githubusercontent.com/thebecoming/cc/master/equipTurtle.lua equipTurtle")
     
     shell.run("delete t")
     shell.run("wget https://raw.githubusercontent.com/thebecoming/cc/master/t.lua t")
-    shell.run("delete gomine")
-    shell.run("wget https://raw.githubusercontent.com/thebecoming/cc/master/gomine.lua gomine")
     shell.run("delete digline")
     shell.run("wget https://raw.githubusercontent.com/thebecoming/cc/master/digline.lua digline")
-    shell.run("delete digstair")
-    shell.run("wget https://raw.githubusercontent.com/thebecoming/cc/master/digstair.lua digstair") 
-
     shell.run("delete startup")
     shell.run("wget https://raw.githubusercontent.com/thebecoming/cc/master/startDigline.lua startup")
-    -- shell.run("delete placeblocks")
-    -- shell.run("wget https://raw.githubusercontent.com/thebecoming/cc/master/placeblocks.lua placeblocks")
-    -- shell.run("delete seafill")
-    -- shell.run("wget https://raw.githubusercontent.com/thebecoming/cc/master/seafill.lua seafill")
     
 end
 
