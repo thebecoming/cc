db1eedd187959cc22347c80f22e4691a4a2083c7
thebecoming <derekyoussi@gmail.com>
2018-08-11 21:45:28 -0700

    tmp

--- startDigline.lua
+++ startDigline.lua
@@ -1,4 +1,4 @@
-shell.run("delete pullDigline")
-shell.run("wget https://raw.githubusercontent.com/thebecoming/cc/master/pullDigline.lua pullDigline")
-shell.run("pullDigline")
+-- shell.run("delete pullDigline")
+-- shell.run("wget https://raw.githubusercontent.com/thebecoming/cc/master/pullDigline.lua pullDigline")
+-- shell.run("pullDigline")
 shell.run("digline")
--- startGomine.lua
+++ startGomine.lua
@@ -1,4 +1,4 @@
-shell.run("delete pullGomine")
-shell.run("wget https://raw.githubusercontent.com/thebecoming/cc/master/pullGomine.lua pullGomine")
-shell.run("pullGomine")
+-- shell.run("delete pullGomine")
+-- shell.run("wget https://raw.githubusercontent.com/thebecoming/cc/master/pullGomine.lua pullGomine")
+-- shell.run("pullGomine")
 shell.run("gomine")
